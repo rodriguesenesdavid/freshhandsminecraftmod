cb2b53d92ad9aeb7cf904c8ffcc59701757a7b15
rodriguesenesdavid <rodriguesenesdavid@gmail.com>
2026-07-03 19:41:43 +0100

    Add files via upload

---
+++ src/client/java/com/itzdavidpt/freshhands/client/FreshHandsClient.java
@@ -0,0 +1,10 @@
+package com.itzdavidpt.freshhands.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FreshHandsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
